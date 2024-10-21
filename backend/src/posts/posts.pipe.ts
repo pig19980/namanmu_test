97c3a559f2e245d6f9427934d2c3a1ff15357dc7
pig19980 <pig19980@kaist.ac.kr>
2024-10-21 23:30:06 +0900

    feat: exception factory 수정

--- backend/src/posts/posts.pipe.ts
+++ backend/src/posts/posts.pipe.ts
@@ -1,17 +1,29 @@
-import { ArgumentMetadata, Injectable, PipeTransform, ValidationPipe } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadGatewayException,
+  Injectable,
+  PipeTransform,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Injectable()
 export class PostsPipe implements PipeTransform {
   async transform(value: any, metadata: ArgumentMetadata) {
     console.log(value, metadata);
     const validPipe = new ValidationPipe({
-      disableErrorMessages: true,
+      disableErrorMessages: false,
+      exceptionFactory: (errors: ValidationError[]) => {
+        console.log(errors);
+        return new BadGatewayException('custom error');
+      },
     });
     try {
       const val = await validPipe.transform(value, metadata);
       console.log(val, '????');
       return val;
     } catch (error) {
+      console.log(error);
       console.log('catched in local pipe');
       throw error;
     }
